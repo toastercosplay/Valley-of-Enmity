54f3df3a19b4144a09b127cba01845f0463b280c
toastercosplay <andrewdouglas2005@gmail.com>
2026-03-05 12:21:10 -0800

    game management stuff / 4 players

--- Assets/GameManagement/Scripts/ShowTotalScores.cs
+++ Assets/GameManagement/Scripts/ShowTotalScores.cs
@@ -5,15 +5,21 @@ public class ShowTotalScores : MonoBehaviour
 {
     PlayerData player1Data;
     PlayerData player2Data;
+    PlayerData player3Data;
+    PlayerData player4Data;
 
     [SerializeField] TextMeshProUGUI player1ScoreText;
     [SerializeField] TextMeshProUGUI player2ScoreText;
+    [SerializeField] TextMeshProUGUI player3ScoreText;
+    [SerializeField] TextMeshProUGUI player4ScoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player2Data = GameObject.FindGameObjectWithTag("Player2Data").GetComponent<PlayerData>();
         player1Data = GameObject.FindGameObjectWithTag("Player1Data").GetComponent<PlayerData>();
+        player3Data = GameObject.FindGameObjectWithTag("Player3Data").GetComponent<PlayerData>();
+        player4Data = GameObject.FindGameObjectWithTag("Player4Data").GetComponent<PlayerData>();
     }
 
     // Update is called once per frame
@@ -21,5 +27,13 @@ public class ShowTotalScores : MonoBehaviour
     {
         player1ScoreText.text = player1Data.GetTotalScore().ToString();
         player2ScoreText.text = player2Data.GetTotalScore().ToString();
+        if (player3Data!= null)
+        {
+            player3ScoreText.text = player3Data.GetTotalScore().ToString();
+        }
+        if (player4Data != null)
+        {
+            player4ScoreText.text = player4Data.GetTotalScore().ToString();
+        }
     }
 }
